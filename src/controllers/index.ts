2a57f5ea5b2b2589dec905507887642989dc4d8b
apozel <tomchaillan@gmail.com>
2023-02-09 21:13:57 +0100

    ne build toujours pas mais l'essentiel est la

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,4 @@
-import { Request, Response } from "express";
+import BlagueController from "./blague";
 
-/**
- * GET /
- * Home page.
- */
-export const index = async (req: Request, res: Response): Promise<void> => {
-    res.render("index", { title: "Express" });
-};
+
+export {BlagueController}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,4 +3,4 @@ import * as controller from "../controllers/index";
 
 export const index = Router();
 
-index.get("/", controller.index);
+index.get("/api/blague", controller.BlagueController.returnRandomJoke);
