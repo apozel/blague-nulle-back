a280fa7506c44e4baa97e4fffd52085a0d3c8651
apozel <tomchaillan@gmail.com>
2023-03-25 15:02:38 +0100

    random blague & server up and running

--- src/exception/handler.ts
+++ src/exception/handler.ts
@@ -1,17 +1,85 @@
-import { NextFunction, Request, Response } from "express";
-import createError from "http-errors";
+import { Application } from 'express';
 
-declare type WebError = Error & { status?: number };
-export const errorHandler = (err: WebError, req: Request, res: Response, next: NextFunction): void => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+import Log from '../middlewares/Log';
+import Locals from '../providers/locals';
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error", { title: err.name, message: err.message });
-};
+class Handler {
+    /**
+     * Handles all the not found routes
+     */
+    public static notFoundHandler(_express: Application): any {
+        const apiPrefix = Locals.config().apiPrefix;
 
-export const errorNotFoundHandler = (req: Request, res: Response, next: NextFunction): void => {
-    next(createError(404));
-};
+        _express.use('*', (req, res) => {
+            const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+            Log.error(`Path '${req.originalUrl}' not found [IP: '${ip}']!`);
+            if (req.xhr || req.originalUrl.includes(`/${apiPrefix}/`)) {
+                return res.json({
+                    error: 'Page Not Found'
+                });
+            } else {
+                res.status(404);
+                return res.render('pages/error', {
+                    title: 'Page Not Found',
+                    error: []
+                });
+            }
+        });
+
+        return _express;
+    }
+
+    /**
+     * Handles your api/web routes errors/exception
+     */
+    public static clientErrorHandler(err: any, req: any, res: any, next: any): any {
+        Log.error(err.stack);
+
+        if (req.xhr) {
+            return res.status(500).send({ error: 'Something went wrong!' });
+        } else {
+            return next(err);
+        }
+    }
+
+    /**
+     * Show undermaintenance page incase of errors
+     */
+    public static errorHandler(err: any, req: any, res: any, next: any): any {
+        Log.error(err.stack);
+        res.status(500);
+
+        const apiPrefix = Locals.config().apiPrefix;
+        if (req.originalUrl.includes(`/${apiPrefix}/`)) {
+
+            if (err.name && err.name === 'UnauthorizedError') {
+                const innerMessage = err.inner && err.inner.message ? err.inner.message : undefined;
+                return res.json({
+                    error: [
+                        'Invalid Token!',
+                        innerMessage
+                    ]
+                });
+            }
+
+            return res.json({
+                error: err
+            });
+        }
+
+        return res.render('pages/error', { error: err.stack, title: 'Under Maintenance' });
+    }
+
+    /**
+     * Register your error / exception monitoring
+     * tools right here ie. before "next(err)"!
+     */
+    public static logErrors(err: any, req: any, res: any, next: any): any {
+        Log.error(err.stack);
+
+        return next(err);
+    }
+}
+
+export default Handler;
